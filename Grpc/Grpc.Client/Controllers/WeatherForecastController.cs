8467250a5b8783d2253b713ba4cd6dc3620b37e0
WangJunZzz <510423039@qq.com>
2020-01-10 09:17:59 +0800

    init

---
+++ Grpc/Grpc.Client/Controllers/WeatherForecastController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrderServices;
+
+namespace Grpc.Client.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly OrderGrpcService.OrderGrpcServiceClient orderGrpcServiceClient;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger,OrderGrpcService.OrderGrpcServiceClient orderGrpcServiceClient)
+        {
+            _logger = logger;
+            this.orderGrpcServiceClient = orderGrpcServiceClient;
+        }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var result = orderGrpcServiceClient.GetOrderById(new RequestItem() { OrderId = Guid.NewGuid().ToString() });
+            return Ok(result);
+        }
+    }
+}
---
+++ Grpc/Grpc.Server/Grpc/OrderService.cs
@@ -0,0 +1,21 @@
+﻿using Grpc.Core;
+using OrderServices;
+using System;
+using System.Threading.Tasks;
+
+namespace Grpc.Server.Grpc
+{
+    public class OrderService : OrderGrpcService.OrderGrpcServiceBase
+    {
+        public override Task<OrderInfoDTO> GetOrderById(RequestItem request, ServerCallContext context)
+        {
+            OrderInfoDTO order = new OrderInfoDTO();
+            order.OrderId = request.OrderId;
+            order.ProductId = Guid.NewGuid().ToString();
+            order.ProductName = "Grpc";
+            order.Price = "1250";
+            order.Number = 2;
+            return Task.FromResult(order);
+        }
+    }
+}
